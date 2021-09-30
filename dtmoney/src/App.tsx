7779c4dffa54e29fbb657580c57fff32f33ffc3f
Arilson Souza <arilsons61@gmail.com>
2021-09-29 22:24:32 -0300

    Add global styles

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
       <h1>DtMoney</h1>
+
+      <GlobalStyle />
     </div>
   );
 }
