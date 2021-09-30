7cb466f04f5ac507e693d6c539656919be5de8b4
Arilson Souza <arilsons61@gmail.com>
2021-09-30 18:20:10 -0300

    Add transaction type buttons

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 Modal.setAppElement('#root');
 
@@ -11,6 +14,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen: isOpen, onRequestClose: onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -28,6 +33,28 @@ export function NewTransactionModal({ isOpen: isOpen, onRequestClose: onRequestC
         <input placeholder="Título" />
 
         <input type='number' placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type='button'
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor='green'
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type='button'
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor='red'
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
