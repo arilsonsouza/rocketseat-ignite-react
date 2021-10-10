f8f6f3cca43f907aaa34719b0dd2b1dbc0917adb
Arilson Souza <arilsons61@gmail.com>
2021-10-10 11:51:32 -0300

    Add authentication with next-auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,20 @@
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { FaGithub } from 'react-icons/fa';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       className={styles.signInButton}
       type='button'
+      onClick={() => signOut()}
     >
       <FaGithub color='#04D361' />
-      Arilson Souza
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
 
@@ -20,6 +22,7 @@ export function SignInButton() {
     <button
       className={styles.signInButton}
       type='button'
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#EBA417' />
       Sign in with Github
