e7220654fa3a36d594a2da4deb1a29a77f3d93fb
Arilson Souza <arilsons61@gmail.com>
2021-10-21 21:39:39 -0300

    Add sign in page

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,8 +1,64 @@
 import type { NextPage } from 'next';
+import { Flex, Input, Button, FormLabel, FormControl, Stack } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection='column'
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
