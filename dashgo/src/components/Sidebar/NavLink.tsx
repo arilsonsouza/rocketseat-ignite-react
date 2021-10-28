e1e87a080c0149071b9c60a44ef263558ff4d841
Arilson Souza <arilsons61@gmail.com>
2021-10-27 22:02:53 -0300

    separating sidebar in components

---
+++ dashgo/src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,21 @@
+import { ElementType } from 'react';
+import { Link, Text, Icon, LinkProps } from '@chakra-ui/react';
+
+interface NavLinkProps extends LinkProps {
+  icon: ElementType,
+  children: string
+}
+
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+  return (
+    <Link
+      display="flex"
+      alignContent="stretch"
+      py="1"
+      {...rest}
+    >
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="medium">{children}</Text>
+    </Link>
+  );
+}
