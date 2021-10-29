b31149d301376091a529170e5f73c75c37e73f10
Arilson Souza <arilsons61@gmail.com>
2021-10-29 12:19:59 -0300

    Finish app navigation

--- dashgo/src/components/Sidebar/NavLink.tsx
+++ dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,21 +1,28 @@
+import Link from 'next/link';
 import { ElementType } from 'react';
-import { Link, Text, Icon, LinkProps } from '@chakra-ui/react';
+import { Link as ChakraLink, Text, Icon, LinkProps } from '@chakra-ui/react';
 
+import { ActiveLink } from '../ActiveLink';
 interface NavLinkProps extends LinkProps {
+  href: string,
   icon: ElementType,
   children: string
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ href, icon, children, ...rest }: NavLinkProps) {
   return (
-    <Link
-      display="flex"
-      alignContent="stretch"
-      py="1"
-      {...rest}
-    >
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink
+        display="flex"
+        alignContent="stretch"
+        py="1"
+        {...rest}
+      >
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
+
+
   );
 }
