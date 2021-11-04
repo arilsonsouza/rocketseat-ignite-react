c1b1211a5b2ab3a2799e7260a7ea776fa0fa2681
Arilson Souza <arilsons61@gmail.com>
2021-11-04 20:50:32 -0300

    Prefetch user data

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,12 +1,14 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import { Box, Flex, Heading, Text, Button, Icon, Table, Thead, Tbody, Tr, Td, Th, Checkbox, useBreakpointValue, Spinner } from '@chakra-ui/react';
+import { Link, Box, Flex, Heading, Text, Button, Icon, Table, Thead, Tbody, Tr, Td, Th, Checkbox, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 import { useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
 
 const Users: NextPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -16,6 +18,16 @@ const Users: NextPage = () => {
     base: false,
     lg: true
   });
+
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`/users/${userId}`);
+
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -31,7 +43,7 @@ const Users: NextPage = () => {
               {!isLoading && isFetching &&
                 <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -41,7 +53,7 @@ const Users: NextPage = () => {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -71,7 +83,11 @@ const Users: NextPage = () => {
                       <Td px={["4", "4", "6"]}><Checkbox colorScheme="pink" /></Td>
                       <Td>
                         <Box>
-                          <Text fontWeight="bold">{user.name}</Text>
+                          <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                            <Text fontWeight="bold">
+                              {user.name}
+                            </Text>
+                          </Link>
                           <Text fontSize="sm" color="gray.300">{user.email}</Text>
                         </Box>
                       </Td>
