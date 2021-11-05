10f7d73bd5493b87b4c6cfefb0ce836e16005d89
Arilson Souza <arilsons61@gmail.com>
2021-11-04 21:08:56 -0300

    Use mutations

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -19,7 +19,7 @@ const Users: NextPage = () => {
     lg: true
   });
 
-  async function handlePrefetchUser(userId: number) {
+  async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(['user', userId], async () => {
       const response = await api.get(`/users/${userId}`);
 
