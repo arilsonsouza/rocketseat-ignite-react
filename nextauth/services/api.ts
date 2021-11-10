77f07b2981690f8a1c6c566709ecdf4870295ec9
Arilson Souza <arilsons61@gmail.com>
2021-11-09 22:13:24 -0300

    Getting user data from api

--- nextauth/services/api.ts
+++ nextauth/services/api.ts
@@ -1,5 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import { parseCookies } from 'nookies';
 
-export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+const cookies = parseCookies();
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.request.use((config: AxiosRequestConfig) => {
+  config.headers['Authorization'] = `Bearer ${cookies['nextauth.token']}`;
+  return config;
+})
+
+export { api };
