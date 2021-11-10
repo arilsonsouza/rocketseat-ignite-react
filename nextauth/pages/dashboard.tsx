73cab2e4010a26658fc116efc2c3d4379852049a
Arilson Souza <arilsons61@gmail.com>
2021-11-09 22:46:41 -0300

    Refreshing tokens

--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuhtContext } from '../contexts/AuthContext';
+import { api } from '../services/api';
 
 const Dashboard: NextPage = () => {
   const { user } = useContext(AuhtContext);
+  useEffect(() => {
 
+    (async () => {
+      try {
+        const { data } = await api.get('/me');
+        const { email, permissions, roles } = data;
+      } catch (error) {
+        console.log(error);
+      }
+
+    })();
+
+  }, []);
   return (
     <div>
       <h1>Dashboard</h1>
