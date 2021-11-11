023d790675cda61c8b799923219f5b9fd5a35004
Arilson Souza <arilsons61@gmail.com>
2021-11-10 21:53:01 -0300

    Add useCan hook

--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -1,12 +1,15 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import { useContext, useEffect } from 'react';
 import { AuhtContext } from '../contexts/AuthContext';
+import { useCan } from '../hooks/useCan';
 import { setupApiclient } from '../services/api';
 import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 const Dashboard: NextPage = () => {
   const { user } = useContext(AuhtContext);
+  const userCanSeeMetrics = useCan({ permissions: ['metrics.list'] });
+
   useEffect(() => {
 
     (async () => {
@@ -24,6 +27,10 @@ const Dashboard: NextPage = () => {
     <div>
       <h1>Dashboard</h1>
       <p>{user?.email}</p>
+
+      {userCanSeeMetrics && <div>
+        Métricas
+      </div>}
     </div>
   );
 }
